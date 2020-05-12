9701e21d86c907a52126ae727f5e6e9d8c3fe259
Carlos Leilson Pereira Guedes <Carlos.Guedes@sicoob.com.br>
2020-05-12 17:16:08 -0300

    Segunda desafio do rocketseat.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,61 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repositorie);
+
+  response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs  } = request.body;
+
+  const indexRepository = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(indexRepository < 0 ){
+      return response.status(400).json({error:'Repositorio não encontrado.'});
+  }
+
+  const repositorie = { id, title, url, techs, likes: repositories[indexRepository].likes };
+
+  repositories[indexRepository] = repositorie;
+
+  return response.json(repositorie); 
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if(projectIndex < 0 ){
+      return response.status(400).json({error:'Repositorio não encontrado.'});
+  }
+
+  repositories.splice(projectIndex,1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const indexRepository = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(indexRepository < 0 ){
+      return response.status(400).json({error:'Repositorio não encontrado.'});
+  }
+
+  repositories[indexRepository].likes += 1;
+  return response.json( repositories[indexRepository] ); 
 });
 
 module.exports = app;
